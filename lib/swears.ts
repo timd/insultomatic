4120a0f16d7492cf3d9ce8b1da115eb28283d6d3
Tim Duckett <tim@duckett.de>
2022-07-10 17:58:44 +0200

    cleans up insults

--- lib/swears.ts
+++ lib/swears.ts
@@ -1,13 +1,43 @@
-import { copyFileSync } from 'fs';
-import prefixes from './pre_insults.json';
-import suffixes from './suff_insults.json';
-
 export function createInsult() {
-  
-  const randomPrefix = prefixes[Math.floor(Math.random() * prefixes.length)];
-  const randomSuffix = suffixes[Math.floor(Math.random() * suffixes.length)];
 
-  const insult  = randomPrefix.pre + randomSuffix.suff;
+  const prefixArray = [
+    'cum',    'snot',   'spit',   'jizz',  'piss',
+    'puke',   'spunk',  'fart',   'shit',  'crap',
+    'poop',   'poo',    'turd',   'butt',  'ass',
+    'bum',    'dick',   'cock',   'twat',  'knob',
+    'pecker', 'prick',  'pussy',  'tit',   'fuck',
+    'wank',   'dumb',   'lame',
+    'douche', 'jerk',  'dork',
+    'creep',  'dip', 'geek',  'slut',
+    'whore',  'bitch',  'lard',   'scum',  'slime',
+    'dirt',   'skeeze', 'sleaze', 'dog',   'monkey',
+    'rat',    'weasel', 'bird', 'lib',
+    'right',  'soy',    'trump',  'stink', 'bastard',
+    'donkey', 'moron',  'idiot',  'loser', 'harpy',
+  ];
+
+  const suffixArray = [
+    'wad',     'bag',     'bucket', 'sack',    'ball',
+    'splash',  'pot',     'pile',   'weasel',  'monkey',
+    'dog',     'rat',     'bird',   'goblin',  'puffin',
+    'licker',  'muncher', 'fucker', 'sucker',  'lord',
+    'clown',   'slut',    'pirate', 'bro',     'nazi',
+    'whore',   'boy',     'boi',    'burglar', 'bandit',
+    'bitch',   'waffle',  'burger', 'cake',    'nugget',
+    'rag',     'nozzle',  'stain',  'wagon',   'weed',
+    'hat',     'canoe',   'boat',   'stick',   'socket',
+    'trumpet', 'mitten',  'twat',   'dick',    'cunt',
+    'ass',     'cock',    'balls',  'face',    'head',
+    'nuts',    'brain',   'brains', 'knuckle', 'mouth',
+    'nose',    'skull',   'tard',   'oid',     'ster',
+    'azoid',   'let',     'fuck',   'shit',    'fart',
+    'wit',     'lib',     'breath'
+  ];
+
+  const randomPrefix = prefixArray[Math.floor(Math.random() * prefixArray.length)];
+  const randomSuffix = suffixArray[Math.floor(Math.random() * suffixArray.length)];
+
+  const insult  = randomPrefix + randomSuffix;
 
   return { insult: insult };
 
